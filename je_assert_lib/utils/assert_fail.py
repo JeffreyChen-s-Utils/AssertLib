f6ec954f4bb9c6f22cd27d8522c06bd86ae90911
JE-Chen <zenxcvwait@gmail.com>
2023-03-29 18:02:38 +0800

    Add more assert
    Add more assert

--- je_assert_lib/utils/assert_fail.py
+++ je_assert_lib/utils/assert_fail.py
@@ -1,6 +1,2 @@
-from sys import stderr
-
-
 def assert_fail_message(when_failure_print_message: str):
-    print(when_failure_print_message, flush=True, file=stderr)
-    raise AssertionError
+    raise AssertionError(when_failure_print_message)
