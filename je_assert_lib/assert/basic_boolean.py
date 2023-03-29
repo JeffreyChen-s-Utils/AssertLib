85ba2e52ed486c8740afb19e7be42e5630be2cc5
JE-Chen <zenxcvwait@gmail.com>
2023-03-29 13:43:38 +0800

    Add basic assert
    Add basic assert

--- je_assert_lib/assert/basic_boolean.py
+++ je_assert_lib/assert/basic_boolean.py
@@ -1,13 +1,37 @@
-import sys
+from sys import stderr
 
 
-def assert_true(assert_object1, when_failure_print_message):
-    if (bool(assert_object1)) is not True:
-        print(when_failure_print_message, flush=True, file=sys.stderr)
+def assert_true(assert_object1, when_failure_print_message: str):
+    if (bool(assert_object1)) is False:
+        print(when_failure_print_message, flush=True, file=stderr)
         raise AssertionError(locals())
 
 
-def assert_false(assert_object1, assert_object2, when_failure_print_message):
+def assert_false(assert_object1, assert_object2, when_failure_print_message: str):
     if (bool(assert_object1)) is True:
-        print(when_failure_print_message, flush=True, file=sys.stderr)
+        print(when_failure_print_message, flush=True, file=stderr)
+        raise AssertionError(locals())
+
+
+def assert_is(assert_object1, assert_object2, when_failure_print_message: str):
+    if assert_object1 is not assert_object2:
+        print(when_failure_print_message, flush=True, file=stderr)
+        raise AssertionError(locals())
+
+
+def assert_is_not(assert_object1, assert_object2, when_failure_print_message: str):
+    if assert_object1 is assert_object2:
+        print(when_failure_print_message, flush=True, file=stderr)
+        raise AssertionError(locals())
+
+
+def assert_is_none(assert_object1, when_failure_print_message: str):
+    if assert_object1 is not None:
+        print(when_failure_print_message, flush=True, file=stderr)
+        raise AssertionError(locals())
+
+
+def assert_is_not_none(assert_object1, when_failure_print_message: str):
+    if assert_object1 is None:
+        print(when_failure_print_message, flush=True, file=stderr)
         raise AssertionError(locals())
--- je_assert_lib/assert/compare.py
+++ je_assert_lib/assert/compare.py
@@ -1,13 +1,13 @@
-import sys
+from sys import stderr
 
 
-def assert_equal(assert_object1, assert_object2, when_failure_print_message):
-    if not (assert_object1 == assert_object2):
-        print(when_failure_print_message, flush=True, file=sys.stderr)
+def assert_equal(assert_object1, assert_object2, when_failure_print_message: str):
+    if assert_object1 != assert_object2:
+        print(when_failure_print_message, flush=True, file=stderr)
         raise AssertionError(locals())
 
 
-def assert_not_equal(assert_object1, assert_object2, when_failure_print_message):
+def assert_not_equal(assert_object1, assert_object2, when_failure_print_message: str):
     if assert_object1 == assert_object2:
-        print(when_failure_print_message, flush=True, file=sys.stderr)
+        print(when_failure_print_message, flush=True, file=stderr)
         raise AssertionError(locals())
